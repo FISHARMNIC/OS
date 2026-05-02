d8e1c13db2850531e08c79b0bb3ba9bf2b621a7f
FISHARMNIC <nicolas.quijano773@gmail.com>
2026-05-02 15:04:16 -0600

    window render

---
+++ userspace/src/rila/wserver/render.c
@@ -0,0 +1,128 @@
+#include "inc/wserver.h"
+#include "inc/window.h"
+
+#include <userspace/fs.h>
+#include <userspace/malloc.h>
+#include <userspace/stdio.h>
+#include <userspace/vbuff.h>
+
+static graphics_context_t ctx;
+static framebuffer_t render_buffer_fb;
+static uint8_t *render_buffer = NULLPTR;
+
+void panic()
+{
+    printf("[RENDER PANIC] Unable to continue rendering. Halting.\n");
+    while (1)
+        ;
+    // @todo
+}
+
+static void swap_buffers()
+{
+    uint8_t *front = (uint8_t *)framebuffer.addr;
+    uint8_t *back = (uint8_t *)render_buffer_fb.addr;
+    uint32_t size = framebuffer.pitch * framebuffer.height;
+
+    for (uint32_t i = 0; i < size; i++)
+    {
+        front[i] = back[i];
+    }
+}
+
+render_init_errors_t render_init()
+{
+    fd_t font_fd;
+    uint32_t err = ffind(&font_fd, "SYS/FONT.F16"); // @todo move to fonts/font.f16
+    if (err)
+    {
+        return RIN_ERR_FONT;
+    }
+
+    uint32_t font_size = fsize(&font_fd);
+    ctx.font = malloc(font_size);
+    if (ctx.font == NULLPTR)
+    {
+        return RIN_ERR_FONT_MALLOC;
+    }
+
+    if (fread(&font_fd, (char *)ctx.font, font_size) != font_size)
+    {
+        free((uint8_t *)ctx.font);
+        ctx.font = NULLPTR;
+        return RIN_ERR_FONT_READ;
+    }
+
+    uint32_t render_buffer_size = framebuffer.pitch * framebuffer.height;
+    render_buffer = malloc(render_buffer_size);
+    if (render_buffer == NULLPTR)
+    {
+        free((uint8_t *)ctx.font);
+        ctx.font = NULLPTR;
+        return RIN_ERR_BUFFER_MALLOC;
+    }
+
+    render_buffer_fb = framebuffer;
+    render_buffer_fb.addr = (uint32_t)render_buffer;
+
+    ctx.color_fg = COLOR_BLACK;
+    ctx.fb = &render_buffer_fb;
+
+    err = desktop_init();
+    if (err != RIN_OK)
+    {
+        free(render_buffer);
+        free((uint8_t *)ctx.font);
+        render_buffer = NULLPTR;
+        ctx.font = NULLPTR;
+        return err;
+    }
+
+    return RIN_OK;
+}
+
+void render_deinit()
+{
+    desktop_deinit();
+    free((uint8_t *)ctx.font);
+    free(render_buffer);
+}
+
+void render()
+{
+    clear_screen(BG_COLOR, &render_buffer_fb);
+
+    desktop_render(&ctx);
+
+    // window_render(&(window_t){
+    //                   .x = 100,
+    //                   .y = 100,
+    //                   .event = NULLPTR,
+    //                   .flags = 0,
+    //                   .title = "TEST",
+    //                   .width = 400,
+    //                   .height = 300},
+    //               0, 0, &ctx);
+    // window_render(&(window_t){
+    //                   .x = 120,
+    //                   .y = 120,
+    //                   .event = NULLPTR,
+    //                   .flags = 0,
+    //                   .title = "TEST",
+    //                   .width = 400,
+    //                   .height = 300},
+    //               0, 0, &ctx);
+    // window_render(&(window_t){
+    //                   .x = 140,
+    //                   .y = 140,
+    //                   .event = NULLPTR,
+    //                   .flags = 0,
+    //                   .title = "TEST",
+    //                   .width = 400,
+    //                   .height = 300},
+    //               0, 0, &ctx);
+
+    mouse_render(&render_buffer_fb);
+
+    swap_buffers();
+}
---
+++ userspace/src/rila/wserver/window.c
@@ -0,0 +1,99 @@
+#include "inc/wserver.h"
+#include <userspace/vbuff.h>
+
+static window_selected_t wasClicked;
+
+static uint32_t window_bar_color(window_t *window)
+{
+    if (wasClicked.ptr == window)
+    {
+        return COLOR_WHITE;
+    }
+
+    return COLOR_FORMAT_RGB(110, 170, 110);
+}
+
+static void draw_bordered_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color, uint32_t border_color, uint32_t border_width, framebuffer_t *fb)
+{
+    fill_rect(x, y, width, height, color, fb);
+
+    for (uint32_t i = 0; i < border_width; i++)
+    {
+        fill_rect(x + i, y + i, width - i * 2, 1, border_color, fb);
+        fill_rect(x + i, y + height - 1 - i, width - i * 2, 1, border_color, fb);
+        fill_rect(x + i, y + i, 1, height - i * 2, border_color, fb);
+        fill_rect(x + width - 1 - i, y + i, 1, height - i * 2, border_color, fb);
+    }
+}
+
+static void draw_transparent_char(uint8_t character, uint32_t x, uint32_t y, graphics_context_t *ctx)
+{
+    const uint8_t *glyph = ctx->font + (character * CHAR_HEIGHT);
+
+    for (uint32_t offy = 0; offy < CHAR_HEIGHT; offy++)
+    {
+        for (uint32_t offx = 0; offx < CHAR_WIDTH; offx++)
+        {
+            if ((glyph[offy] & MASK(CHAR_WIDTH - 1 - offx)) != 0)
+            {
+                put_pixel(x + offx, y + offy, ctx->color_fg, (framebuffer_t*)ctx->fb);
+            }
+        }
+    }
+}
+
+static void draw_transparent_string(char *string, uint32_t x, uint32_t y, graphics_context_t *ctx)
+{
+    while (*string)
+    {
+        draw_transparent_char((uint8_t)*string, x, y, ctx);
+        x += CHAR_WIDTH;
+        string++;
+    }
+}
+
+static void widget_draw_all(window_t *window, int32_t rel_click_x, int32_t rel_click_y)
+{
+    (void)window;
+    (void)rel_click_x;
+    (void)rel_click_y;
+}
+
+static void window_events_trigger_expose(window_t *window)
+{
+    (void)window;
+}
+
+void window_draw(window_t *window, int32_t rel_click_x, int32_t rel_click_y, graphics_context_t *ctx)
+{
+    uint32_t x = window->x;
+    uint32_t y = window->y;
+    uint32_t width = window->width;
+    uint32_t height = window->height;
+    uint32_t saved_fg = ctx->color_fg;
+
+    ctx->color_fg = COLOR_BLACK;
+
+    draw_bordered_rect(x, y, width, WINDOW_BAR_HEIGHT, window_bar_color(window), COLOR_BLACK, 1, (framebuffer_t*)ctx->fb);
+    draw_transparent_string("- + X", x + width - 50, y + 2, ctx);
+    draw_transparent_string(window->title, x + 5, y + 2, ctx);
+    draw_bordered_rect(x, y + WINDOW_BAR_HEIGHT - 1, width, height, /*window->window_ctx.color_fg*/ COLOR_FORMAT_RGB(73, 177, 230), COLOR_BLACK, 1, (framebuffer_t*)ctx->fb);
+    draw_bordered_rect(x + width - WINDOW_RESIZE_AREA, y + WINDOW_BAR_HEIGHT + height - WINDOW_RESIZE_AREA - 1, WINDOW_RESIZE_AREA, WINDOW_RESIZE_AREA, COLOR_FORMAT_RGB(160, 160, 160), COLOR_BLACK, 1, (framebuffer_t*)ctx->fb);
+
+    ctx->color_fg = saved_fg;
+
+    widget_draw_all(window, rel_click_x, rel_click_y);
+}
+
+void window_render(window_t *window, int32_t rel_click_x, int32_t rel_click_y, graphics_context_t *ctx)
+{
+    window_draw(window, rel_click_x, rel_click_y, ctx);
+    window_events_trigger_expose(window);
+}
+
+void window_render_all(int32_t click_x, int32_t click_y, graphics_context_t *ctx)
+{
+    (void)click_x;
+    (void)click_y;
+    (void)ctx;
+}
